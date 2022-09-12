491e0bca9a3b17780f08b1c4a3f26919da35d91d
electrovir <electrovir@users.noreply.github.com>
2022-09-12 21:41:31 +0000

    add jaden casing implementation

--- src/2022-09-12-jaden-casing-strings/jaden-casing-strings.ts
+++ src/2022-09-12-jaden-casing-strings/jaden-casing-strings.ts
@@ -1,4 +1,6 @@
 // https://www.codewars.com/kata/5390bac347d09b7da40006f6
 export function toJadenCase(input: string): string {
-    return '';
+    return input.toLowerCase().replace(/^(\w)|(\s\w)/g, (capturedGroup) => {
+        return capturedGroup.toUpperCase();
+    });
 }
