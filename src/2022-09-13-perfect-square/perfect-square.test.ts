079d906abe0bd0a7b1c2fe34f48cbb9d645c0372
electrovir <electrovir@users.noreply.github.com>
2022-09-14 17:57:37 +0000

    add tests for perfect square

---
+++ src/2022-09-13-perfect-square/perfect-square.test.ts
@@ -0,0 +1,43 @@
+import {describe, it} from 'mocha';
+import {assertOutput} from '../test-helpers/assert-function-output';
+import {isPerfectSquare} from './perfect-square';
+
+describe(isPerfectSquare.name, () => {
+    it('should pass test cases', () => {
+        type TestCase = {
+            input: number;
+            expect: boolean;
+        };
+
+        const testCases: TestCase[] = [
+            {
+                input: -1,
+                expect: false,
+            },
+            {
+                input: 0,
+                expect: true,
+            },
+            {
+                input: 3,
+                expect: false,
+            },
+            {
+                input: 4,
+                expect: true,
+            },
+            {
+                input: 25,
+                expect: true,
+            },
+            {
+                input: 26,
+                expect: false,
+            },
+        ];
+
+        testCases.forEach((testCase) => {
+            assertOutput(isPerfectSquare, testCase.expect, testCase.input);
+        });
+    });
+});
---
+++ src/2022-09-13-perfect-square/perfect-square.ts
@@ -0,0 +1,4 @@
+// https://www.codewars.com/kata/54c27a33fb7da0db0100040e
+export function isPerfectSquare(input: number): boolean {
+    return false;
+}
