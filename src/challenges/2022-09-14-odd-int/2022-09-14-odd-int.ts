c49b6779ed60a9058fed31434306d0bba196cbf7
electrovir <electrovir@users.noreply.github.com>
2022-09-14 19:08:31 +0000

    finish odd int

--- src/challenges/2022-09-14-odd-int/2022-09-14-odd-int.ts
+++ src/challenges/2022-09-14-odd-int/2022-09-14-odd-int.ts
@@ -1,4 +1,24 @@
-// https://www.codewars.com/kata/54da5a58ea159efa38000836
-export function oddInt(input: any) {
-    return input;
+import {getObjectTypedKeys} from 'augment-vir';
+
+// https://www.codewars.com/kata/LINK_HERE
+export function oddInt(input: number[]): number | undefined {
+    const instanceCounts: Record<number, number> = {};
+    input.forEach((entry) => {
+        if (entry in instanceCounts) {
+            instanceCounts[entry]++;
+        } else {
+            instanceCounts[entry] = 1;
+        }
+    });
+
+    const oddNumber = getObjectTypedKeys(instanceCounts).find((key) => {
+        const count = instanceCounts[key] ?? 0;
+        return count % 2;
+    });
+
+    if (oddNumber == undefined) {
+        return undefined;
+    } else {
+        return Number(oddNumber);
+    }
 }
