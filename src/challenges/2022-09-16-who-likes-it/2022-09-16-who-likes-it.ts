b489270e8296e768c7f2eb82db20eb9766c2c2a1
electrovir <electrovir@users.noreply.github.com>
2022-09-16 14:42:59 +0000

    add whoLikesIt implementation

--- src/challenges/2022-09-16-who-likes-it/2022-09-16-who-likes-it.ts
+++ src/challenges/2022-09-16-who-likes-it/2022-09-16-who-likes-it.ts
@@ -1,4 +1,27 @@
 // https://www.codewars.com/kata/5266876b8f4bf2da9b000362
-export function whoLikesIt(input: string[]): string {
-    return '';
+export function whoLikesIt(inputs: string[]): string {
+    return `${determineList(inputs)} ${getSuffix(inputs)}`;
+}
+
+function determineList(inputs: string[]): string {
+    if (inputs.length > 0 && inputs.length < 3) {
+        return inputs.join(' and ');
+    } else if (inputs.length >= 3) {
+        const trailing = inputs.length === 3 ? inputs[2] : `${inputs.length - 2} others`;
+
+        return [
+            inputs.slice(0, 2).join(', '),
+            trailing,
+        ].join(' and ');
+    } else {
+        return 'no one';
+    }
+}
+
+function getSuffix(inputs: string[]): string {
+    if (inputs.length < 2) {
+        return 'likes this';
+    } else {
+        return 'like this';
+    }
 }
