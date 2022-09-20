65fb61a721e786fa13d4da9ad1afb532279a9ffb
electrovir <electrovir@users.noreply.github.com>
2022-09-20 16:51:15 -0600

    Array diff (#5)
    * add array-diff tests
    
    * add array-diff implementation

---
+++ src/challenges/2022-09-20-array-diff/2022-09-20-array-diff.ts
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/523f5d21c841566fde000009
+export function arrayDiff(
+    startingArray: ReadonlyArray<number>,
+    removeTheseValues: ReadonlyArray<number>,
+): ReadonlyArray<number> {
+    return startingArray.filter((startingEntry) => !removeTheseValues.includes(startingEntry));
+}
